284c408d76cff4c529a5411910ed3ed11b8d212e
liangmiangliang <2902753024@qq.com>
2021-04-08 10:35:46 -0700

    find.c cannot exec

---
+++ user/find.c
@@ -0,0 +1,94 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+
+
+
+
+
+void find(char *dirName, char *fileName)
+{
+	char buf[512], *p;
+	int fd;
+	struct dirent de;
+	struct stat st;
+	if ((fd = open(dirName, 0)) < 0)
+	{
+		fprintf(2, "find: cannot open %s\n", dirName);
+		return;
+	}
+	
+	if (fstat(fd, &st) < 0)
+	{
+		fprintf(2, "find: cannot stat %s\n", dirName);
+		close(fd);
+		return;
+	}
+
+	if (st.type != T_DIR)
+	{
+		fprintf(2, "find: %s is not a directory\n", dirName);
+		close(fd);
+		return;
+	}
+
+	if (strlen(dirName) + 1 + DIRSIZ + 1 > sizeof buf)
+	{
+		fprintf(2, "find: %s name is too long\n" dirName);
+		close(fd);
+		return;
+	}
+
+	strcpy(buf, dirName);
+	p = buf + strlen(buf);
+	*p++ = '/';
+	while (read(fd, &de, sizeof(de)) == sizeof(de))
+	{
+		if (de.inum == 0)
+		{
+			continue;
+		}
+
+		if (!strcmp(de.name, ".") || !strcmp(de.name, ".."))
+		{
+			continue;
+		}
+
+		memmove(p, de.name, DIRSIZ);
+		p[DIRSIZ] = 0;
+		if (stat(buf, &st) < 0)
+		{
+			fprintf(2, "find: cannot stat %s\n", buf);
+			continue;
+		}
+
+		if (st.type == T_DIR)
+		{
+			find(buf, fileName);
+		}
+		else if ((st.type == T_FILE) && !strcmp(de.name, fileName))
+		{
+			printf("%s\n", buf);
+		}
+	}
+
+}
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		fprintf(2, "usage: find dirName fileName!\n");
+		exit(1);
+	}
+
+	find(argv[1], argv[2]);
+	exit(0);
+}
